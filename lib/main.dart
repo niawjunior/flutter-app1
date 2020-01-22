1f11b865c6157f9dc2e5fb0b7471873f5b1cccbd
niawjunior <pasupol@dome.cloud>
2020-01-22 14:22:21 +0700

    Using StatelessWidget (MyApp)

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text('Flutter App'),
-        backgroundColor: Colors.grey[900],
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage('images/cat2.jpg'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('Flutter App'),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/cat2.jpg'),
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
